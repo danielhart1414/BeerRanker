df93619ec2c4c2d120461c849087dc65a5eaa8e8
danielhart1414 <daniel_hart@live.com>
2019-09-13 23:01:40 -0400

    Revised remove, favorite, and constructor methods

--- src/beerranker/BeerList.java
+++ src/beerranker/BeerList.java
@@ -1,5 +1,5 @@
 //@author Daniel Hart
-//Constructs a beer list to be used by BeerRanker
+//A beer list to be used by BeerRanker
 //
 //Fix needed: error-catching code at line 34.
 
@@ -17,8 +17,14 @@ public class BeerList {
 
     public BeerList(Scanner input) {
         this();
-        while (input.hasNextLine()) {
-            myBeers.add(input.nextLine());
+        if (input.hasNextLine()) {
+            String firstBeer = input.nextLine();
+            if (!firstBeer.equals("You have no beer :(")) {
+                myBeers.add(firstBeer);
+                while (input.hasNextLine()) {
+                    myBeers.add(input.nextLine());
+                }
+            }
         }
     }
 
@@ -27,7 +33,7 @@ public class BeerList {
             System.out.println("It's already on the list.");
         } else if (!myBeers.isEmpty()) {
             Scanner console = new Scanner(System.in);
-            int n = 0;
+            int n;
             for (;;) {
                 System.out.println("Please type the number of the beer it's better than");
                 System.out.print("(\"0\" if it's the worst). ");
@@ -50,38 +56,43 @@ public class BeerList {
     }
 
     public void remove(String beer) {
-            if (myBeers.contains(beer)) {
-                    myBeers.remove(beer);
-            } else {
-                    System.out.println("It's not on here, kiddo");
-            }
+        if (myBeers.contains(beer)) {
+            myBeers.remove(beer);
+        } else {
+            System.out.println("That's not on the list, kiddo.");
+        }
     }
 
     public String fav() {
-            if (!myBeers.isEmpty()) {
-                    return myBeers.get(0);
-            } else {
-                    return "You don't have any beer, laddy!";
-            }
+        if (!myBeers.isEmpty()) {
+            return myBeers.get(0) + " is your favorite beer.";
+        } else {
+            return "You haven't ranked any beer, laddy!";
+        }
     }
 
     public void write(PrintStream output) {
-            if (myBeers.size() != 0) {
-                    for (int i = 0; i < myBeers.size(); i++) {
-                            output.println(myBeers.get(i));
-                    }
-            } else {
-                    output.println("You have no beer :(");
+        if (myBeers.size() != 0) {
+            for (int i = 0; i < myBeers.size(); i++) {
+                output.println(myBeers.get(i));
             }
+        } else {
+            output.println("You have no beer :(");
+        }
     }
 
     public void print() {
-            if (myBeers.size() != 0) {
-                    for (int i = 0; i < myBeers.size(); i++) {
-                            System.out.println((i + 1) + ". " + myBeers.get(i));
-                    }
-            } else {
-                    System.out.println("You have no beer :(");
+        if (myBeers.size() != 0) {
+            for (int i = 0; i < myBeers.size(); i++) {
+                System.out.println((i + 1) + ". " + myBeers.get(i));
             }
+        } else {
+            System.out.println("You have no beer :("); //Remove code on line 64 primaryFunctions
+            //and revise showBeerList for when it's empty.
+        }
+    }
+    
+    public boolean isEmpty() {
+        return myBeers.isEmpty();
     }
 }
