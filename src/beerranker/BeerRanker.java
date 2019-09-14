daecdeb1bc96c6225ab407e57b56c43a29003f58
danielhart1414 <daniel_hart@live.com>
2019-09-13 22:07:34 -0400

    Consolidated loadList; made methods and fileName static

--- src/beerranker/BeerRanker.java
+++ src/beerranker/BeerRanker.java
@@ -12,41 +12,56 @@ import java.util.*;
 import java.io.*;
 
 public class BeerRanker {
+    static String fileName;
+    
     public static void main(String[] args) throws FileNotFoundException {
         Scanner console = new Scanner(System.in);
-        BeerList list = new BeerList();
         System.out.println("Welcome to Beer Ranker!");
         System.out.println();
+        BeerList beers = loadList(console);
+        solve(console, beers);
+    }
+    
+    private static BeerList loadList(Scanner console) throws FileNotFoundException {
+        BeerList myBeers;
+        String s;
         for (;;) {
             System.out.print("Have you ranked beers before? (Answer \"y\" or \"n\") ");
-            String s = console.nextLine();
+            s = console.nextLine();
             if (s.trim().toLowerCase().equals("y")) {
-                System.out.print("What file holds the beer that you've ranked? ");
-                //needs error catch
-                String fileName = console.nextLine();  
-                Scanner input = new Scanner(new File(fileName));
-                list = new BeerList(input);
-                solve(console, list, fileName);
+                Scanner input;
+                for(;;) {
+                    System.out.print("What file holds the beer that you've ranked? ");
+                    fileName = console.nextLine();
+                    try {
+                        input = new Scanner(new File(fileName));
+                        break;
+                    } catch (FileNotFoundException e) {
+                        System.out.println("That file does not exist.");
+                    }
+                }
+                myBeers = new BeerList(input);
                 break;
-            } else if (!s.trim().toLowerCase().equals("n")) {
+            } else if (s.trim().toLowerCase().equals("n")) {
+                System.out.print("What text file would you like to store your new beer list to? ");
+                fileName = console.nextLine();
+                myBeers = new BeerList();
+                break;
+            } else {
                 System.out.println("You failed to answer y or n.");
                 System.out.println("Please try again.");
-            } else {
-                System.out.print("What text file would you like to store your new list to? ");
-                String fileName = console.nextLine();
-                solve(console, list, fileName);
-                break;
             }
         }
+        return myBeers;
     }
 	
-    private static void solve(Scanner console, BeerList list, String fileName) 
+    private static void solve(Scanner console, BeerList beers) 
             throws FileNotFoundException {
         for (;;) {
             System.out.println();
             System.out.println("Here are your beers:"); //This should only print when a beer list exists
             System.out.println();
-            list.print();
+            beers.print();
             System.out.println();
             System.out.println("What would you like to do? (type the appropriate number)");
             System.out.println("1. Add a beer");
@@ -58,18 +73,18 @@ public class BeerRanker {
             System.out.println();
             if (s.equals("1")) {
                 System.out.print("What beer would you like to add? ");
-                list.addBeer(console.nextLine());
+                beers.add(console.nextLine());
             } else if (s.equals("2")) {
                 System.out.print("What beer would you like to remove? (Type the name of the beer) ");
-                list.removeBeer(console.nextLine());
+                beers.remove(console.nextLine());
             }	else if (s.equals("3")) {
-                System.out.println(list.fav());
+                System.out.println(beers.fav());
             } else if (s.equals("4")) {
                 break;
             } else {
                 System.out.println("The numbers are near the top of the keyboard");
             }
         }
-        list.write(new PrintStream(new File(fileName)));
+        beers.write(new PrintStream(new File(fileName)));
     }
 }
